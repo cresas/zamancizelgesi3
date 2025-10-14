6213b8ce8fa444ee68442654c08fc98c29be9d91
cresas <137892152+cresas@users.noreply.github.com>
2025-10-14 06:49:45 +0300

    Add files via upload

---
+++ panel/upload/temp/8047.js
@@ -0,0 +1,81 @@
+{
+  "scale": "human",
+  "events": [
+    {
+      "start_date": {"year": "1946", "month": "12", "day": ""},
+      "text": {
+        "headline": "Çinhindi Savaşı’nın Başlangıcı",
+        "text": "2 Eylül 1945'te Hanoi'da, Viet Minh lideri Ho Chi Minh Vietnam Demokratik Cumhuriyeti'ni ilan etti. 1946'nın Aralık ayında Fransız Seferi Kuvvetleri ile Viet Minh arasında gerilla savaşı başladı."
+      },
+      "media": {"url": "img/1.jpg"}
+    },
+
+    {
+      "start_date": {"year": "1954", "month": "7", "day": "21"},
+      "text": {
+        "headline": "Çinhindi Savaşı'nın Sonu",
+        "text": "Cenevre Konferansı, Fransa ve Viet Minh arasındaki savaşa son verdi. Vietnam ikiye bölündü: Kuzey Vietnam komünist kontrolüne girerken, Güney Vietnam milliyetçi bir yönetim altında kaldı."
+      },
+      "media": {"url": "img/2.jpg"}
+    },
+    {
+      "start_date": {"year": "1961", "month": "4", "day": "17"},
+      "end_date": {"year": "1961", "month": "4", "day": "19"},
+      "text": {
+        "headline": "Domuzlar Körfezi Çıkarması",
+        "text": "ABD, Fidel Castro karşıtı Kübalı sürgünleri destekleyerek Domuzlar Körfezi'ne bir çıkarma düzenledi. Ancak operasyon başarısız oldu."
+      },
+      "media": {"url": "img/3.jpg"}
+    },
+    {
+      "start_date": {"year": "1962", "month": "10", "day": "16"},
+      "end_date": {"year": "1962", "month": "10", "day": "28"},
+      "text": {
+        "headline": "Küba Buhranı",
+        "text": "Sovyetler Birliği'nin Küba'ya yerleştirdiği füzeler nedeniyle ABD ile SSCB arasında nükleer savaş riski yaşandı. Uzlaşmayla kriz sona erdi."
+      },
+      "media": {"url": "img/4.png"}
+    },
+    {
+      "start_date": {"year": "1965", "month": "3", "day": "2"},
+      "text": {
+        "headline": "ABD’nin Vietnam’a Askerî Saldırı Başlatması",
+        "text": "ABD, Kuzey Vietnam’ı sistematik bir şekilde bombalamaya başladı ve Güney Vietnam’a büyük ölçüde asker sevk etti. 1968'de Vietnam'daki ABD kuvvetlerinin sayısı 500.000'i aştı."
+      },
+      "media": {"url": "img/5.jpg"}
+    },
+    {
+      "start_date": {"year": "1965", "month": "8", "day": "5"},
+      "end_date": {"year": "1965", "month": "9", "day": "23"},
+      "text": {
+        "headline": "Keşmir Sorunu",
+        "text": "Pakistan askerlerinin Keşmir’e girmesiyle savaş başladı. 23 Eylül’de BM Güvenlik Konseyi’nin ateşkes kararına uyuldu."
+      },
+      "media": {"url": "img/6.jpg"}
+    },
+    {
+      "start_date": {"year": "1973", "month": "1", "day": "27"},
+      "text": {
+        "headline": "Paris’te Vietnam Antlaşması’nın İmzalanması",
+        "text": "Vietnam için 1968'de başlayan barış görüşmeleri 27 Ocak 1973'te Paris’te sonuçlandı. ABD'nin Kuzey Vietnam'a saldırıları sona erdirildi."
+      },
+      "media": {"url": "img/7.jpg"}
+    },
+    {
+      "start_date": {"year": "1979", "month": "12", "day": "24"},
+      "text": {
+        "headline": "Sovyetler Birliği’nin Afganistan'ı İşgali",
+        "text": "SSCB, Afganistan'daki komünist rejimi desteklemek için ülkeyi işgal etti. Mücahitlerle 1988'e kadar sürecek bir savaş başladı."
+      },
+      "media": {"url": "img/8.jpg"}
+    },
+    {
+      "start_date": {"year": "1988", "month": "4", "day": "14"},
+      "text": {
+        "headline": "SSCB’nin Afganistan’dan Çekilmeye Başlaması",
+        "text": "SSCB, Cenevre Antlaşması kapsamında 15 Şubat 1989’a kadar Afganistan’dan çekilmeyi taahhüt etti."
+      },
+      "media": {"url": "img/9.jpg"}
+    }
+  ]
+}
